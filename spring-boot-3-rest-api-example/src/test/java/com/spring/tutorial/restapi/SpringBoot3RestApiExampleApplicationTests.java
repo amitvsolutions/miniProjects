14385c9aee42c967764fea47b3970fa85f1ea8ab
amitv\devmachine <amitverma.atpl@gmail.com>
2023-08-20 20:14:45 +0530

    spring rest docker example

---
+++ spring-boot-3-rest-api-example/src/test/java/com/spring/tutorial/restapi/SpringBoot3RestApiExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.tutorial.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3RestApiExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
